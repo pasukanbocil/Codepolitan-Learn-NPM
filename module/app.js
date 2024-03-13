4b92c886a3ff88eacb58ede35b3646e0c0cbb9f5
pasukanbocil <ctian8787@gmail.com>
2024-03-13 13:22:53 +0700

    docs: :lipstick: MEL MEL

---
+++ module/app.js
@@ -0,0 +1,8 @@
+// const rumus = require("./rumus");
+
+const { penjumlahan, PI, luas, keliling } = require("./rumus");
+
+console.log(penjumlahan(1, 2));
+console.log(PI);
+console.log(luas(10, 5));
+console.log(keliling(7));
